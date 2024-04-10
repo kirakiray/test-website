fe1c8b5a78423b1b5a212ebf4ba0e8bc8e0ed8a9
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-04-10 16:32:12 +0800

    Update proxy-server.js

--- scripts/proxy-server.js
+++ scripts/proxy-server.js
@@ -1,8 +1,38 @@
-const httpProxy = require("http-proxy");
+var http = require("http");
 
-httpProxy
-  .createServer({
-    target: "http://localhost:11434",
-    changeOrigin: true,
-  })
-  .listen(11001);
+var server = http.createServer(function (req, res) {
+  if (req.method === "OPTIONS") {
+    debugger;
+  }
+
+  var proxyReq = http.request(
+    {
+      hostname: "localhost",
+      port: 11434,
+      path: req.url,
+      method: req.method,
+      //   headers: req.headers,
+      headers: {
+        ...req.headers,
+        // fake request headers
+        origin: "http://localhost:5515",
+        referer: "http://localhost:5515/",
+      },
+    },
+    function (proxyRes) {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      // fake response headers
+      res.writeHead(proxyRes.statusCode, {
+        ...proxyRes.headers,
+        "access-control-allow-origin": req.headers.origin,
+      });
+      proxyRes.pipe(res, { end: true });
+    }
+  );
+
+  req.pipe(proxyReq, { end: true });
+});
+
+server.listen(11001);
+
+console.log("Server running at http://127.0.0.1:11001/");
